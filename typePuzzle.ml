8f1300b36302ef100def474335b3c95c0b76fa91
Nico <nicolasdgy@outlook.com>
2019-04-04 00:16:25 +0200

    Update typePuzzle.ml

--- typePuzzle.ml
+++ typePuzzle.ml
@@ -1,19 +1,24 @@
-type coordinate = int * int;;
+module Type_jeu = struct
+	
 
-type importance = int ;;
+	type coordinate = int * int
 
+	type importance = int 
 
+	type puzzle = (coordinate * importance) list 
 
-type puzzle = (coordinate * importance) list ;;
+	type bridge = { isVertical : bool; isDoubled : bool } 
 
-type bridge = { isVertical : bool; isDoubled : bool } ;;
+	type cell =
+		 Nothing 
+		| Island of importance
+		| Bridge of bridge 
+	
 
-type cell =
-	 Nothing 
-	| Island of importance
-	| Bridge of bridge 
-;;
+	type solution = cell list list
 
-type solution = cell list list ;;
+	type nbPont = int
 
-type data = coordinate list;;
+	type data = coordinate * (importance * nbPont ) list 
+
+end
