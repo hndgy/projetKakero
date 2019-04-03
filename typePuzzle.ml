c6851394bda981149ef7ff35e8e4e21c4f9b4492
Nico <nicolasdgy@outlook.com>
2019-04-03 21:47:45 +0200

    Create typePuzzle.ml

---
+++ typePuzzle.ml
@@ -0,0 +1,17 @@
+type coordinate = int * int;;
+
+type importance = int ;;
+
+
+
+type puzzle = (coordinate * importance) list ;;
+
+type bridge = { isVertical : bool; isDoubled : bool } ;;
+
+type cell =
+	 Nothing 
+	| Island of importance
+	| Bridge of bridge 
+;;
+
+type solution = cell list list ;;
