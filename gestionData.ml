399aa27db65b8593e3aed28c442f30bb628eb682
Nico <nicolasdgy@outlook.com>
2019-04-04 00:16:18 +0200

    Update gestionData.ml

--- gestionData.ml
+++ gestionData.ml
@@ -1,12 +1,26 @@
 
-
 module Gestion_data = 
 struct
-	type d = TypePuzzle.data 
-	type c = TypePuzzle.coordinate
 
-	val empty : unit -> d
-	val add : c -> d
-	val contains : c -> bool
-	
-end
+	let init p =
+		 let rec aux p acc =
+    		match p with
+				[] -> acc
+      			| (c,imp) :: t -> aux t (acc@[(c,(imp,0))])
+  		in aux p []
+
+
+	let contains ldata coord = 
+		let rec aux ldata acc =
+			match ldata with
+				[] -> acc
+				| (c,(i,n)) :: t -> if c = coord then aux t true
+			else aux t false
+		in 
+		aux ldata false
+
+
+
+end;;
+
+
