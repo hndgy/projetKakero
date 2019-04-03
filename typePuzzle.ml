84acaeb4b36bc03086370e6fb67e56ae709f6ecc
Nico <nicolasdgy@outlook.com>
2019-04-04 00:16:37 +0200

    Update typePuzzle.ml

--- typePuzzle.ml
+++ typePuzzle.ml
@@ -1,24 +1,25 @@
-module Type_jeu = struct
-	
 
-	type coordinate = int * int
+type coordinate = int * int
 
-	type importance = int 
+type importance = int 
 
-	type puzzle = (coordinate * importance) list 
+type puzzle = (coordinate * importance) list 
 
-	type bridge = { isVertical : bool; isDoubled : bool } 
+type bridge = { isVertical : bool; isDoubled : bool } 
 
-	type cell =
+type cell =
 		 Nothing 
 		| Island of importance
 		| Bridge of bridge 
 	
 
-	type solution = cell list list
+type solution = cell list list
 
-	type nbPont = int
+type nbPont = int
 
-	type data = coordinate * (importance * nbPont ) list 
+type data = coordinate * (importance * nbPont ) list 
 
-end
+type liaison = 
+	Null 
+	| Voisin of Voisin * liaison
+
