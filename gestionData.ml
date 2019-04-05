fcc456d94549893e80f7a4b9da15e26b0ccf54dd
Nico <nicolasdgy@outlook.com>
2019-04-05 10:24:01 +0200

    Update gestionData.ml

--- gestionData.ml
+++ gestionData.ml
@@ -1,13 +1,12 @@
+module Data = struct
 
-module Gestion_data = 
-struct
 
 	let init p =
 		 let rec aux p acc =
-    		match p with
+	   		match p with
 				[] -> acc
-      			| (c,imp) :: t -> aux t (acc@[(c,(imp,0))])
-  		in aux p []
+	      		| (c,imp) :: t -> aux t (acc@[(c,(imp,0))])
+	  	in aux p []
 
 
 	let contains ldata coord = 
@@ -15,12 +14,28 @@ struct
 			match ldata with
 				[] -> acc
 				| (c,(i,n)) :: t -> if c = coord then aux t true
-			else aux t false
+						else aux t false
 		in 
 		aux ldata false
 
 
 
-end;;
+
+	let set_nbPont ldata coord nbPont =
+		let rec aux ldata acc=
+			match ldata with
+			[] -> acc
+			| ((c,(i,n)) as h) :: t  -> if c = coord then aux t acc@[(c,(i,nbPont))]
+				else aux t acc@[h]
+		in
+		aux ldata acc 
+		
+
+
+
+end 
+
+
+
 
 
