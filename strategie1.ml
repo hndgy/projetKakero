678742b40f2fea8abf2e47619a12d231acddd07d
Nico <nicolasdgy@outlook.com>
2019-04-05 10:24:09 +0200

    Update strategie1.ml

---
+++ strategie1.ml
@@ -0,0 +1,14 @@
+open TypePuzzle;;
+
+module Strategie1 : Strategie_type = struct
+	type d = data 
+
+	let resoudre ldata =
+		let rec aux l acc =
+			match l with
+			| [] -> acc
+			| (c,) :: t -> match
+		in
+
+
+end
