5b8f45556dee620555f3c8939e1d44f6443e9a22
Nico <nicolasdgy@outlook.com>
2019-04-17 17:11:36 +0200

    Update database.ml

--- database.ml
+++ database.ml
@@ -5,54 +5,40 @@ module Data = struct
 
 
     
-	let init pzz =
+	let init pzz = 
+	(*initialise un type database avec un puzzle*)
 		 let rec aux p acc =
 	   		match p with
 				[] -> acc
 	      		| (c,imp) :: t -> aux t (acc@[(c,(imp,0),(Puzzle.ensVoisin pzz c))])
+	      		(*pour chaque sommet, on créée un trilpet avec les coordonnés, l'importance,
+	      		le nombre de pont connectés, et la liste des voisins disponibles *)
 	  	in ((aux pzz []),[])
 
 
-(*	let contains ldata coord = 
-		let rec aux ldata acc =
-			match ldata with
-				[] -> acc
-				| (c,(i,n)) :: t -> if c = coord then aux t true
-						else aux t false
-		in 
-		aux ldata false*)
-
-        
-
-
-
-
-
-	
-       
 
 
 	let control_collision database (x1,y1) (x2,y2) nb =
-	(*retire une ile de la liste des voisins si il y a un pont entre*)
+	(*controle si le pont qu'on ajoute entre (x1,y1) (x2,y2) coupe des liaisons 
+entre certain sommets*)
 
 
-          let cond (xa,ya) (xb,yb) =
+          let cond (xa,ya) (xb,yb) = (*condition pour qu'une liaison entre deux sommets
+          soit rompue par un pont créé*)
 
 	    if x1 = x2 && ya = yb then 
 	      (((xa < x1) && (x1 < xb)) || ((xa > x1) &&( x1 > xb) )) &&
 		(((y1 < ya) &&( ya< y2) ) || ((y1 < ya) && (ya< y2) ))
-	    else
-
-	      (((x1 < xa) && (xa < x2)) || ((x1 > xa) && (xa > x2))) &&
+	    else if y1 = y2 && xa = xb then 
+	    (((x1 < xa) && (xa < x2)) || ((x1 > xa) && (xa > x2))) &&
 		(((ya < y1) && (y1 < yb)) || ((ya > y1) && (y1 > yb)))
-		
-		
-		
+		else false 
 		
 	  in
 
 	  
-	  let supprCollision c lv =
+	  let supprCollision c lv = (*supprime de la listes des voisins lv les sommets qui ne sont plus 
+	  voisin à cause de la création d'un pont entre*)
 	    let rec aux lv acc =
 	      match lv with
 		  [] -> acc
@@ -62,17 +48,19 @@ module Data = struct
 	    aux lv []
 	  in
 
-	  let f (c,d,lv) = (c,d,(supprCollision c lv)) in
+	  let f (c,d,lv) = (c,d,(supprCollision c lv)) in (*applique le la suppression à une composante 
+	  de la liste*)
 
 	  let ldata = fst database and hist = snd database in
 
-	  let del ldata = List.map f ldata in 
+	  let del ldata = List.map f ldata in (*applique la suppression à tous les elements de la liste*)
 	  
 	  ((del ldata),hist)
 		
 	      
 
 	let update datab =
+	(*met à jour la database: supprime les sommet traité, ie ou le nombre de pont vaut l'importance*)
 	  let ldata = fst datab and data = snd datab in
 	  
 	  let update_aux ldata =
@@ -103,9 +91,7 @@ module Data = struct
 	      (c,d,aux l [])
 	    in
 
-	    
-
-	 (*supprime les elements supprimer des liste de voisin de chaque sommets restants*)
+	 (*supprime les elements supprimés des liste de voisin de chaque sommets restants*)
 	    let rec aux1 supp acc =
 	      match supp with
 		  [] -> acc
@@ -116,21 +102,29 @@ module Data = struct
 
 	(update_aux ldata,data)
 
-	let connect database nb coord1 coord2  =
 
-	  let database' = control_collision database coord1 coord2 nb in
+	exception NbPontIncorrect
+
+	let connect database nb coord1 coord2  =
+	(*connect deux sommets de coordonnés coord1 coord2 avec un nombere nb de ponts*)
+
+	  let database' = control_collision database coord1 coord2 nb in (*on fait *)
 	  let ldata = (fst database') and hist = (snd database') in
 
 	  
 	  
 	  let rec modifNb ldata acc =
+	  (*modifie le nombre de pont connecté pour les deux coorconné*)
 	    match ldata with
 		[] -> acc
 	      | ((c,(i,n),v) as h) :: t  ->
-		if c = coord1 || c = coord2 then modifNb t acc@[(c,(i,n+nb),v)]
+		if (c = coord1 || c = coord2) && (nb + n) <= i then modifNb t acc@[(c,(i,n+nb),v)]
+		else if (c = coord1 || c = coord2) && (nb + n) > i then raise NbPontIncorrect
 		else modifNb t acc@[h]
 	  in
-	  let newHist =
+	  let newHist = (*ajoute l'opération dans l'historique :
+	  si il y a deja un pont de connecté alors on ajoute un autre pont 
+	  sinon on ajoute un element (coord1,coord2,nb)*)
 	    if (List.mem (coord1,coord2,1) hist) || (List.mem (coord2,coord1,1) hist) then
 
 	      let rec aux hist acc =
@@ -148,27 +142,16 @@ module Data = struct
 	  
 	  in
 		  
-	  let newDb = ((modifNb ldata []), newHist) in
+	  try update ((modifNb ldata []), newHist) with NbPontIncorrect -> database
+	  (*retourne la nouvelle database si le nb de pont est possible à ajouter sinon on retourne 
+	  celle donnée en argument *)
 	  
-	  update newDb
+	   
 
 
 
-
-
-	
-	let getNbVoisin datab coord =
-	  let ldata = fst datab in
-	  let rec aux l acc =
-	    match l with
-		[] -> acc
-	      | (c,d,l) :: t -> if c = coord then aux t (List.length l) else aux t acc
-	  in if ldata = [] then raise Empty_list
-	    else 
-	      aux ldata 0
-
-
-	let map_connect c ldata lv n = (*applique une incr. de "n" a toute la liste de voisin "lv" de "c"*)
+	let map_connect c ldata lv n = 
+	(*ajoute n pont entre c et ses voisins*)
 	  let rec aux lv acc=
 	    match lv with
 		[] -> acc
@@ -176,48 +159,10 @@ module Data = struct
 	  in
 	  aux lv ldata
 
-	  
-	let isConnexe database =
-	  let hist = snd database in
-
-	  let isConnected coord hist =
-	    let rec aux hist acc =
-	      match hist with
-		  [] -> acc
-		| (c1,c2,_) :: t -> if coord = c1 || coord = c2 then aux t true
-		  else aux t acc||false
-	    in
-	    aux hist false
-	  in
-
-	  let without d hist =
-	    let rec aux hist acc =
-	      match hist with
-		  [] -> acc
-		| h :: t -> if h = d then aux t acc else aux t acc@[h]
-	    in
-	    aux hist []
-	  in
-	  
-	  let aux_isConnexe =
-	    let rec aux h acc =
-	      match h with
-		  [] -> acc
-		| ((c1,c2,_) as s) :: t ->
-		  let histSansS = without s hist in
-		  if (isConnected c1 histSansS) || (isConnected c2 histSansS) then
-		    aux t (acc&&true)
-		  else aux t false
-	    in
-	    aux hist true
-	  in
-
-	  aux_isConnexe 
+	
 	  
 
-
-
-
+       
 	
 	
 	
